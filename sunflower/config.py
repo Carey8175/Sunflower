7888196bca4b40c10f52fcb4547245842947406a
carey <980328708@qq.com>
2024-12-02 08:20:18 +0800

    some config

--- sunflower/config.py
+++ sunflower/config.py
@@ -8,3 +8,10 @@ HEROES_PATH = os.path.join(STATIC_PATH, "heroes.json")
 AUGMENTS_PATH = os.path.join(STATIC_PATH, "hex.json")
 EVOLUTIONS_PATH = os.path.join(STATIC_PATH, "goop.json")
 IMAGE_PATH = os.path.join(STATIC_PATH, "images")
+
+
+"""
+Some game settings
+"""
+# the interval after each operation, note: the smaller the value, the faster the operation, but with a higher risk
+INTERVAL = 0.3
